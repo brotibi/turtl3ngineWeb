52d5b207770f79a8e5e6b8dceea217b2e7f1e717
Bolaji <holyboboli@javodnicv3.myfiosgateway.com>
2022-01-28 18:07:22 -0500

    Lets get it started in here

---
+++ models/Entity.ts
@@ -0,0 +1,4 @@
+export default interface Entity {
+    update(): void;
+    draw(): void;
+}
---
+++ models/WorldObject.ts
@@ -0,0 +1,12 @@
+import Entity from "./Entity";
+
+export default class WorldObject implements Entity {
+    
+
+    draw(): void {
+    }
+
+    update(): void {
+    }
+
+}
